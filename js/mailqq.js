cb2608d2ce79b3b83f4d50fd87c346bbd726bd30
rmJay <rruinmi1@gmail.com>
2021-04-29 20:37:29 +0800

    更新 qqmail

---
+++ js/mailqq.js
@@ -0,0 +1,11 @@
+try {
+    const reci_box_a = document.querySelector("#folder_1");
+    const logo_a = document.querySelector("#logotips > a");
+    const main_a = document.querySelector("#logotips > div > div > span.addrtitle > a:nth-child(1)");
+    const search_input = document.querySelector("#subject");
+    search_input.value = "";
+    search_input.placeholder = "搜索";
+    main_a.href = reci_box_a.href;
+    logo_a.href = reci_box_a.href;
+    reci_box_a.click();
+} catch (error){};
